44ffa68854ff6aea5f133643471b5781a7f20f24
fuyong <1277145053@qq.com>
2023-10-18 09:52:45 +0800

    add tls

--- common/infrastructure/redis/db.go
+++ common/infrastructure/redis/db.go
@@ -2,6 +2,11 @@ package redis
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -11,13 +16,33 @@ var (
 )
 
 func Init(cfg *Config) error {
+	ca, err := ioutil.ReadFile(cfg.DBCert)
+	if err != nil {
+		return err
+	}
+
+	if err := os.Remove(cfg.DBCert); err != nil {
+		return err
+	}
+
+	pool := x509.NewCertPool()
+	if !pool.AppendCertsFromPEM(ca) {
+		return fmt.Errorf("failed to append certs PEM")
+	}
+
+	tlsConfig := &tls.Config{
+		RootCAs:            pool,
+		InsecureSkipVerify: true,
+	}
+
 	client = redis.NewClient(&redis.Options{
-		Addr:     cfg.Address,
-		Password: cfg.Password,
-		DB:       0,
+		Addr:      cfg.Address,
+		Password:  cfg.Password,
+		DB:        0,
+		TLSConfig: tlsConfig,
 	})
 
-	_, err := client.Ping(context.Background()).Result()
+	_, err = client.Ping(context.Background()).Result()
 	if err != nil {
 		return err
 	}
