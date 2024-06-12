c28e207c6f3011a50d2445990d6c796f57837af6
drizzlezyk <drizzlezyk@163.com>
2024-06-12 10:06:34 +0800

    update message cloud config (#731)

--- cloud/app/message.go
+++ cloud/app/message.go
@@ -14,28 +14,43 @@ type CloudMessageService interface {
 func NewCloudMessageService(
 	repo repository.Pod,
 	manager cloud.CloudPod,
-	survivalTimeForPod int64,
+	survivalTimeForPodCPU int64,
+	survivalTimeForPodAscend int64,
 ) CloudMessageService {
 	return &cloudMessageService{
-		repo:               repo,
-		manager:            manager,
-		survivalTimeForPod: survivalTimeForPod,
+		repo:                     repo,
+		manager:                  manager,
+		survivalTimeForPodCPU:    survivalTimeForPodCPU,
+		survivalTimeForPodAscend: survivalTimeForPodAscend,
 	}
 }
 
 type cloudMessageService struct {
-	repo               repository.Pod
-	manager            cloud.CloudPod
-	survivalTimeForPod int64
+	repo                     repository.Pod
+	manager                  cloud.CloudPod
+	survivalTimeForPodCPU    int64
+	survivalTimeForPodAscend int64
 }
 
 func (c *cloudMessageService) CreatePodInstance(p *domain.PodInfo) error {
 	// create pod instance by SDK
-	logrus.Debugf("send create pod info: %#v", p)
-	err := c.manager.Create(
+	logrus.Infof("send create pod info: %#v", p)
+	survivalTime := p.Expiry.PodExpiry()
+	if p.IsCpu() {
+		survivalTime = c.survivalTimeForPodCPU
+	} else if p.IsAscend() {
+		survivalTime = c.survivalTimeForPodAscend
+	}
+
+	expire, err := domain.NewPodExpiry(survivalTime)
+	if err != nil {
+		return err
+	}
+
+	err = c.manager.Create(
 		&cloud.CloudPodCreateInfo{
 			PodId:        p.Id,
-			SurvivalTime: c.survivalTimeForPod,
+			SurvivalTime: survivalTime,
 			User:         p.Owner.Account(),
 			CloudType:    p.GetCloudType(),
 		},
@@ -47,6 +62,7 @@ func (c *cloudMessageService) CreatePodInstance(p *domain.PodInfo) error {
 
 	// update pod status in DB
 	p.StatusSetCreating()
+	p.Expiry = expire
 
 	return c.repo.UpdatePod(p)
 }
