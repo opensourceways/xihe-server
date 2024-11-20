848ad7130f20e1ad6e2fbfd52387375525166726
yangxinyue-git <2605234659@qq.com>
2024-11-20 16:57:13 +0800

    json

--- filescan/app/dto.go
+++ filescan/app/dto.go
@@ -1,6 +1,6 @@
 package app
 
 type FilescanDTO struct {
-	ModerationStatus string `json:"ModerationStatus"`
-	ModerationResult string `json:"ModerationResult"`
+	ModerationStatus string `json:"moderation_status"`
+	ModerationResult string `json:"moderation_result"`
 }
