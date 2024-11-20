da40748713160a5faa2077069caae862bff33514
yangxinyue-git <2605234659@qq.com>
2024-11-20 11:44:14 +0800

    1

--- filescan/infrastructure/repositoryadapter/filescan.go
+++ filescan/infrastructure/repositoryadapter/filescan.go
@@ -2,33 +2,9 @@ package repositoryadapter
 
 import "github.com/opensourceways/xihe-server/filescan/domain"
 
-// "errors"
-
-// "github.com/opensourceways/xihe-server/common/domain/repository"
-
-// primitive "github.com/opensourceways/xihe-server/domain"
-
 type FileScanAdapter struct {
 	daoImpl
 }
-
-// // FindByRecordIndex finds a record based on the account record index and returns an error if any occurs.
-// func (adapter *FileScanAdapter) FindByRecordIndex(index domain.FileScanIndex) (
-// 	domain.FileScan, error,
-// ) {
-// 	do := fileScanDO{
-// 		Owner:    index.UserName.Account(),
-// 		RepoName: index.RepoName.Account(),
-// 	}
-
-// 	// It must new a new DO, otherwise the sql statement will include duplicate conditions.
-// 	result := fileScanDO{}
-// 	if err := adapter.daoImpl.GetRecord(&do, &result); err != nil {
-// 		return domain.FileScan{}, err
-// 	}
-
-// 	return result.toFileScan(), nil
-// }
 
 func (adapter *FileScanAdapter) Get(owner string, repoName string) ([]domain.FilescanRes, error) {
 
