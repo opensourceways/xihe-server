ed5ae1b30eaab22987d8c544efd83b91953b42bd
yangxinyue-git <2605234659@qq.com>
2024-11-22 11:53:10 +0800

    print filescan,do

--- filescan/app/filescan.go
+++ filescan/app/filescan.go
@@ -3,6 +3,7 @@ package app
 
 import (
 	"context"
+	"fmt"
 
 	filescan "github.com/opensourceways/xihe-server/filescan/domain"
 	repo "github.com/opensourceways/xihe-server/filescan/domain/repository"
@@ -34,12 +35,14 @@ func (s *fileScanService) Get(owner string, repoName string) ([]filescan.Filesca
 // Update updates a file scan.
 func (s *fileScanService) Update(ctx context.Context, cmd CmdToUpdateFileScan) error {
 	fileInfo, err := s.FileScanAdapter.Find(cmd.Id)
+	fmt.Printf("===================fileInfo1: %+v\n", fileInfo)
 
 	if err != nil {
 		return err
 	}
 
 	fileInfo.HandleScanDone(cmd.ModerationResult)
+	fmt.Printf("===================fileInfo2: %+v\n", fileInfo)
 
 	return s.FileScanAdapter.Save(&fileInfo)
 }
