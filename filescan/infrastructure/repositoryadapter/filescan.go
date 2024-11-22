63d4d3080cb706d1969daf99c3ce6d61e5cda005
yangxinyue-git <2605234659@qq.com>
2024-11-22 11:53:10 +0800

    print filescan,do

--- filescan/infrastructure/repositoryadapter/filescan.go
+++ filescan/infrastructure/repositoryadapter/filescan.go
@@ -1,6 +1,10 @@
 package repositoryadapter
 
-import "github.com/opensourceways/xihe-server/filescan/domain"
+import (
+	"fmt"
+
+	"github.com/opensourceways/xihe-server/filescan/domain"
+)
 
 const (
 	fieldId = "id"
@@ -68,6 +72,6 @@ func (adapter *fileScanAdapter) Find(id int64) (domain.FileScan, error) {
 
 func (adapter *fileScanAdapter) Save(file *domain.FileScan) error {
 	do := toFileScanDO(file)
-
+	fmt.Printf("===================do: %+v\n", do)
 	return adapter.db().Where(adapter.daoImpl.EqualQuery(fieldId), do.Id).Save(&do).Error
 }
