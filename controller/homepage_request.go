3b3598ccd181897d7ca7c0fe28aa83e3fedc9f8b
yangxinyue-git <2605234659@qq.com>
2024-12-19 14:26:12 +0800

    update from main

--- controller/homepage_request.go
+++ controller/homepage_request.go
@@ -4,6 +4,7 @@ import (
 	"github.com/opensourceways/xihe-server/app"
 	compapp "github.com/opensourceways/xihe-server/competition/app"
 	courseapp "github.com/opensourceways/xihe-server/course/app"
+	promapp "github.com/opensourceways/xihe-server/promotion/app"
 	spaceapp "github.com/opensourceways/xihe-server/space/app"
 )
 
@@ -13,9 +14,10 @@ type homeInfo struct {
 }
 
 type homeElectricityInfo struct {
-	Comp    []compapp.CompetitionSummaryDTO `json:"comp"`
-	Course  []courseapp.CourseSummaryDTO    `json:"course"`
-	Peoject spaceapp.GlobalProjectsDTO      `json:"project"`
-	Dataset app.GlobalDatasetsDTO           `json:"dataset"`
-	Model   app.GlobalModelsDTO             `json:"model"`
+	Comp       []compapp.CompetitionSummaryDTO `json:"comp"`
+	Course     []courseapp.CourseSummaryDTO    `json:"course"`
+	Promotions []promapp.PromotionDTO          `json:"promotions"`
+	Dataset    app.GlobalDatasetsDTO           `json:"dataset"`
+	Model      app.GlobalModelsDTO             `json:"model"`
+	Peoject    spaceapp.GlobalProjectsDTO      `json:"project"`
 }
