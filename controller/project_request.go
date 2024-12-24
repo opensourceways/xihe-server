56fc19d1358d4f414499bc865b0159f15f2e0a92
yangxinyue-git <2605234659@qq.com>
2024-12-24 15:17:07 +0800

    delete struct

--- controller/project_request.go
+++ controller/project_request.go
@@ -179,16 +179,3 @@ func (req *reqToNotifyUpdateCode) toCmd() (cmd spaceapp.CmdToNotifyUpdateCode, e
 	cmd.CommitId = req.CommitId
 	return
 }
-
-type reqToIncrease struct {
-	Owner string `json:"owner"`
-	Id    string `json:"id"`
-}
-
-func (req *reqToIncrease) toCmd() (cmd spaceapp.CmdToIncrease, err error) {
-	cmd.ResourceIndex = domain.ResourceIndex{
-		Owner: domain.CreateAccount(req.Owner),
-		Id:    req.Id,
-	}
-	return
-}
