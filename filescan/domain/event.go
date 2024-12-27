7ce52ef010c88085c3ec2e6948fdecf34a04ab8e
hdsong2 <holden_s@163.com>
2024-12-27 15:49:40 +0800

    Download API of Repo File Can Be Used to Model and Dataset (#788)
    * build: update package dependencies
    
    * fix(repofile): file url using only repo_id and path
    
    * fix(repofile): get repo file using only repo_id and path
    
    * refactor(repofile): remove `not_recorded` from query
    
    * fix(repofile): conflicts with existing wildcard
    
    * fix(project): access slice with the correct index

--- filescan/domain/event.go
+++ filescan/domain/event.go
@@ -3,6 +3,7 @@ package domain
 type ModerationEvent struct {
 	ID       int64  `json:"id"`
 	Owner    string `json:"owner"`
+	RepoId   int64  `json:"repo_id"`
 	RepoName string `json:"repo_name"`
 	Dir      string `json:"dir"`
 	File     string `json:"file"`
