fd522630ff8fb80de5fe292ff5b8e45bacc7028e
yangxinyue-git <2605234659@qq.com>
2025-01-03 16:32:57 +0800

    list

--- space/infrastructure/repositoryimpl/space.go
+++ space/infrastructure/repositoryimpl/space.go
@@ -360,8 +360,8 @@ func (adapter *projectAdapter) listGlobalAndSortByUpdateTime(
 
 	// 如果 level 字段为空，则使用优先级排序
 	if do.Level == 0 {
-		query = query.Order("CASE WHEN level = 2 THEN 1 WHEN level = 1 THEN 2 WHEN level = 0 THEN 3 END, " +
-			fieldUpdatedAt + "DESC")
+		query = query.Order(
+			"CASE WHEN level = 2 THEN 1 WHEN level = 1 THEN 2 WHEN level = 0 THEN 3 END, " + fieldUpdatedAt + "DESC")
 	} else {
 		// 如果 level 字段不为空，则按照指定的 level 排序
 		query = query.Order(fieldUpdatedAt + "DESC")
@@ -417,8 +417,8 @@ func (adapter *projectAdapter) listGlobalAndSortByDownloadCount(
 
 	// 如果 level 字段为空，则使用优先级排序
 	if do.Level == 0 {
-		query = query.Order("CASE WHEN level = 2 THEN 1 WHEN level = 1 THEN 2 WHEN level = 0 THEN 3 END, " +
-			fieldDownload + "DESC")
+		query = query.Order(
+			"CASE WHEN level = 2 THEN 1 WHEN level = 1 THEN 2 WHEN level = 0 THEN 3 END, " + fieldDownload + "DESC")
 	} else {
 		// 如果 level 字段不为空，则按照指定的 level 排序
 		query = query.Order(fieldDownload + "DESC")
